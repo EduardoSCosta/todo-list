152eb6be26588af0471d208944557beb000f19c4
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-05-16 14:31:30 -0300

    create localStorageService

---
+++ src/services/localStorageService.js
@@ -0,0 +1,47 @@
+const TASK_LOCAL_KEY = 'taskList';
+
+const localStorageService = () => {
+  const get = ({ id }) => {
+    const tasks = getAll();
+    const task = tasks.find(task => task.id === id);
+
+    return task;
+  }
+
+  const getAll = () => {
+    const tasks = localStorage.getItem(TASK_LOCAL_KEY);
+
+    if (tasks === null) {
+      const emptyTasklist = JSON.stringify([]);
+      localStorage.setItem(TASK_LOCAL_KEY, emptyTasklist);
+
+      return JSON.parse(emptyTasklist);
+    } else {
+      return JSON.parse(tasks);
+    }
+  }
+
+  const create = (task) => {
+    const tasks = getAll();
+
+    const listWithNewTask = [...tasks, task];
+    localStorage.setItem(TASK_LOCAL_KEY, JSON.stringify(listWithNewTask));
+
+    return task;
+  }
+
+  const update = ({ id, newStatus }) => {
+    const tasks = getAll();
+    const taskToUpdate = get({ id });
+    const updatedTask = { ...taskToUpdate, status: newStatus };
+
+    const newTaskList = tasks.map(task => task.id === id ? updatedTask : task);
+    localStorage.setItem(TASK_LOCAL_KEY, JSON.stringify(newTaskList));
+
+    return updatedTask;
+  }
+
+  return { get, getAll, create, update };
+}
+
+export default localStorageService;
